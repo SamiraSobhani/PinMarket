2f81d73bc5d240828b4307496f6d41eea1aad78f
Samira Sobhani <ss121280@yahoo.com>
2021-07-02 10:14:33 -0700

    responsive design for search page tablet done

--- client/src/Search.js
+++ client/src/Search.js
@@ -4,22 +4,15 @@ import useApplicationData from "./hooks/useApplicationData";
 import SearchPoster from "./components/SearchPoster/SearchPoster";
 import Timer from "./components/Auth/Timer";
 function Search() {
-  const {
-    coord,
-    setCoord,
-    state,
-    setState,
-    nearPosters,
-    setnearPosters,
-  } = useApplicationData();
+  const { coord, setCoord, state, setState, nearPosters, setnearPosters } =
+    useApplicationData();
   return (
     <appContext.Provider
       value={{ coord, setCoord, state, setState, nearPosters, setnearPosters }}
     >
       <div className=" search__container container">
-        <div>
-          <SearchPoster className="slider" />
-        </div>
+        <SearchPoster className="slider" />
+
         <Timer />
       </div>
     </appContext.Provider>
